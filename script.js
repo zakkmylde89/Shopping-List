27a4217a50e187754ab6042b27a17432f12b66db
zakkmylde89 <47191233+zakkmylde89@users.noreply.github.com>
2024-06-17 09:18:20 -0500

    main functionality complete

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 
 function addItem(e) {
 	e.preventDefault();
@@ -21,7 +22,10 @@ function addItem(e) {
 	const button = createButton('remove-item btn-link text-red');
 	li.appendChild(button);
 
+	// Add li to the DOM
 	itemList.appendChild(li);
+
+	checkUI();
 
 	itemInput.value = '';
 }
@@ -42,7 +46,11 @@ function createIcon(classes) {
 
 function removeItem(e) {
 	if (e.target.parentElement.classList.contains('remove-item')) {
-		e.target.parentElement.parentElement.remove();
+		if (confirm('Are You Sure?')) {
+			e.target.parentElement.parentElement.remove();
+
+			checkUI();
+		}
 	}
 }
 
@@ -50,9 +58,40 @@ function clearItems() {
 	while (itemList.firstChild) {
 		itemList.removeChild(itemList.firstChild);
 	}
+
+	checkUI();
+}
+
+function filterItems(e) {
+	const text = e.target.value.toLowerCase();
+	const items = itemList.querySelectorAll('li');
+
+	items.forEach((item) => {
+		const itemName = item.firstChild.textContent.toLowerCase();
+
+		if (itemName.indexOf(text) != -1) {
+			item.style.display = 'flex';
+		} else {
+			item.style.display = 'none';
+		}
+	});
+}
+
+function checkUI() {
+	const items = itemList.querySelectorAll('li');
+	if (items.length === 0) {
+		clearBtn.style.display = 'none';
+		filter.style.display = 'none';
+	} else {
+		clearBtn.style.display = 'block';
+		filter.style.display = 'block';
+	}
 }
 
 // Event Listeners
 itemForm.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
 clearBtn.addEventListener('click', clearItems);
+itemFilter.addEventListener('input', filterItems);
+
+checkUI();
